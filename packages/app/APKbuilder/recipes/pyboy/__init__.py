9ac19814c57b8ed22fdc646ba3a32082dd639236
Sergio Melero <sergiomele97@gmail.com>
2025-08-16 12:26:10 +0200

    Add pyboy recipe

--- packages/app/APKbuilder/recipes/pyboy/__init__.py
+++ packages/app/APKbuilder/recipes/pyboy/__init__.py
@@ -2,8 +2,8 @@ from pythonforandroid.recipe import PythonRecipe
 
 class PyBoyRecipe(PythonRecipe):
     version = "2.6.0"
-    url = "https://files.pythonhosted.org/packages/source/p/pyboy/pyboy-2.6.0.tar.gz"
-    depends = ["setuptools", "numpy"]  # <-- Aquí agregamos numpy
+    url = "https://github.com/pyboy-for-android/PyBoy/archive/refs/heads/android-2.6.0.zip"
+    depends = ["setuptools", "numpy"]
     call_hostpython_via_targetpython = False
 
 recipe = PyBoyRecipe()
