93c8ad155b3798eeaeae8e14ec3ac813a72941a8
Sergio Melero <sergiomele97@gmail.com>
2025-09-02 07:56:45 +0200

    Comento guardado al salir por posible bug

--- packages/app/APKbuilder/src/main.py
+++ packages/app/APKbuilder/src/main.py
@@ -14,9 +14,9 @@ class MiApp(App):
         sm.add_widget(EmulatorScreen(name='emulator'))
         return sm
 
-    # Runs when leaving Desktop app
-    def on_stop(self):
-        self.root.get_screen('emulator').emulator.save_RAM()
+    # # Runs when leaving Desktop app
+    # def on_stop(self):
+    #     self.root.get_screen('emulator').emulator.save_RAM()
 
 if __name__ == '__main__':
     MiApp().run()
