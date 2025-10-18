4d5fd2ec9fe05678105f712eff8a56db34a82b71
Sergio Melero <sergiomele97@gmail.com>
2025-10-18 17:01:32 +0200

    version conecta a pi

--- packages/server/client_example.py
+++ packages/server/client_example.py
@@ -1,15 +1,22 @@
 import asyncio
 import struct
 import websockets
+import ssl
 
-SERVER_URL = "ws://localhost:8080/ws"
+
+# IMPORTANTE ESTO DE SSL ES UN APAÑO, HAY QUE DEJARLO BIEN
+ssl_context = ssl.create_default_context()
+ssl_context.check_hostname = False
+ssl_context.verify_mode = ssl.CERT_NONE
+
+SERVER_URL = "wss://server_publicado.linkedcrystal.com/ws"
 
 # Empaqueta (ID, X, Y, Z) en formato binario little-endian (igual que Go)
 def make_message(client_id, x, y, z):
     return struct.pack("<4i", client_id, x, y, z)
 
 async def client():
-    async with websockets.connect(SERVER_URL) as ws:
+    async with websockets.connect(SERVER_URL, ssl=ssl_context) as ws:
         print("Conectado al servidor WebSocket")
 
         client_id = 1
