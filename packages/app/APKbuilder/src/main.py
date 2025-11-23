fc1f9354ccfd43426d285ecfa369028a812059b3
Sergio Melero <sergiomele97@gmail.com>
2025-11-23 13:13:26 +0100

    layout global en sucio

--- packages/app/APKbuilder/src/main.py
+++ packages/app/APKbuilder/src/main.py
@@ -1,12 +1,38 @@
 from kivy.app import App
+from kivy.uix.floatlayout import FloatLayout
 from kivy.uix.screenmanager import ScreenManager
+from kivy.clock import Clock
 
 from screens.menu_screen.menu_screen import MenuScreen
 from screens.emulator_screen.emulator_screen import EmulatorScreen
 from services.connection.connection_manager import ConnectionManager
+from global_components.connection_indicator import ConnectionIndicator
 
 class MyScreenManager(ScreenManager):
     pass
+
+class RootWidget(FloatLayout):
+    """Contenedor principal para que el indicador sea global"""
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        # ScreenManager
+        self.sm = MyScreenManager()
+        self.sm.add_widget(MenuScreen(name='bienvenida'))
+        self.sm.add_widget(EmulatorScreen(name='emulator'))
+        self.add_widget(self.sm)
+
+        # Indicador global
+        self.connection_indicator = ConnectionIndicator()
+        self.add_widget(self.connection_indicator)
+
+        # Prueba automática
+        Clock.schedule_once(lambda dt: self.run_demo(), 1)
+
+    def run_demo(self):
+        """Demostración automática de los iconos"""
+        self.connection_indicator.show_ok(duration=5)
+        # Después de 6 segundos (5 + 1 de buffer), mostrar NOK
+        Clock.schedule_once(lambda dt: self.connection_indicator.show_nok(duration=5), 6)
 
 class LinkedCrystalApp(App):
 
@@ -17,12 +43,8 @@ class LinkedCrystalApp(App):
         self.connection_manager = ConnectionManager()
 
     def build(self):
-        sm = MyScreenManager()
-        sm.add_widget(MenuScreen(name='bienvenida'))
-        sm.add_widget(EmulatorScreen(name='emulator'))
-        return sm
+        return RootWidget()
 
 if __name__ == '__main__':
     appInstance = LinkedCrystalApp()
     appInstance.run()
-
