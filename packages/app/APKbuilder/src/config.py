550aa7af054869a0c312b93e787e19382e3bf180
Sergio Melero <sergiomele97@gmail.com>
2025-12-08 12:54:42 +0100

    migracion a config

---
+++ packages/app/APKbuilder/src/config.py
@@ -0,0 +1,7 @@
+# Apuntar a local
+ENV = "local"
+URL = "http://localhost:8080"
+SSL_URL = ""
+STATIC_TOKEN = "demo_token"
+PORT = "8080"
+SERVERS = "localhost"
--- packages/app/APKbuilder/src/env.py
+++ packages/app/APKbuilder/src/env.py
@@ -1,36 +1,10 @@
-from kivy.utils import platform
-
-# Valores por defecto
-ENV = "local"
-URL = "http://localhost:8080"
-SSL_URL = ""
-STATIC_TOKEN = "demo_token"
-PORT = "8080"
-SERVERS = "localhost"
-
-if platform != "android":
-    # Desktop: cargar .env si existe
-    try:
-        from dotenv import load_dotenv
-        load_dotenv()
-        import os
-        ENV = os.getenv("ENV", ENV)
-        URL = os.getenv("URL", URL)
-        SSL_URL = os.getenv("SSL_URL", SSL_URL)
-        STATIC_TOKEN = os.getenv("STATIC_TOKEN", STATIC_TOKEN)
-        PORT = os.getenv("PORT", PORT)
-        SERVERS = os.getenv("SERVERS", SERVERS)
-    except ImportError:
-        print("python-dotenv no instalado, usando valores por defecto")
-else:
-    # Android: importar config.py generado dinámicamente
-    try:
-        from config import ENV as cfg_ENV, URL as cfg_URL, SSL_URL as cfg_SSL_URL, STATIC_TOKEN as cfg_STATIC_TOKEN, PORT as cfg_PORT, SERVERS as cfg_SERVERS
-        ENV = cfg_ENV
-        URL = cfg_URL
-        SSL_URL = cfg_SSL_URL
-        STATIC_TOKEN = cfg_STATIC_TOKEN
-        PORT = cfg_PORT
-        SERVERS = cfg_SERVERS
-    except ImportError:
-        print("config.py no encontrado, usando valores por defecto")
+try:
+    from config import ENV, URL, SSL_URL, STATIC_TOKEN, PORT, SERVERS
+    ENV = ENV
+    URL = URL
+    SSL_URL = SSL_URL
+    STATIC_TOKEN = STATIC_TOKEN
+    PORT = PORT
+    SERVERS = SERVERS
+except ImportError:
+    print("config.py no encontrado, usando valores por defecto")
