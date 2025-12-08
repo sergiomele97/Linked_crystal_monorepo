c6a8ef4d4ea1ca011797aa404c0c5f68981a0858
Sergio Melero <sergiomele97@gmail.com>
2025-12-08 12:29:26 +0100

    solucion a bug botones atascados

--- packages/app/APKbuilder/src/screens/emulator_screen/components/controlpad.py
+++ packages/app/APKbuilder/src/screens/emulator_screen/components/controlpad.py
@@ -3,19 +3,30 @@ from kivy.properties import ObjectProperty
 from kivy.uix.image import Image
 from kivy.uix.behaviors import ButtonBehavior
 
-
 class ImageButton(ButtonBehavior, Image):
     pass
-
 
 class ControlPad(FloatLayout):
     on_button_press = ObjectProperty(None)
     on_button_release = ObjectProperty(None)
 
+    # Mantener un estado interno para los botones presionados
+    _pressed_buttons = set()
+
     def press(self, button_name):
-        if self.on_button_press:
-            self.on_button_press(button_name)
+        # Solo registrar si no está ya presionado
+        if button_name not in self._pressed_buttons:
+            self._pressed_buttons.add(button_name)
+            if self.on_button_press:
+                self.on_button_press(button_name)
 
     def release(self, button_name):
-        if self.on_button_release:
-            self.on_button_release(button_name)
+        if button_name in self._pressed_buttons:
+            self._pressed_buttons.remove(button_name)
+            if self.on_button_release:
+                self.on_button_release(button_name)
+
+    def release_all(self):
+        """Liberar todos los botones que puedan haberse quedado pegados."""
+        for btn in list(self._pressed_buttons):
+            self.release(btn)
